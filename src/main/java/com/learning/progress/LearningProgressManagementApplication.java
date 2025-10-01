7ae5d7b817c59bbb0be29ea698a982592ab20151
mthutt2k3 <mthutt2k3@gmai.com>
2025-10-01 10:18:42 +0700

    initial code

---
+++ src/main/java/com/learning/progress/LearningProgressManagementApplication.java
@@ -0,0 +1,13 @@
+package com.learning.progress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningProgressManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningProgressManagementApplication.class, args);
+	}
+
+}
